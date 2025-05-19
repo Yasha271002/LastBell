c0c8c339cfe6474ce025ec02a284538de337bab1
Яков Бобров <yasha27102@yandex.ru>
2025-05-19 17:48:29 +0400

    Add project files.

---
+++ LastBell/HostBuilders/BuildMainNavigationExtension.cs
@@ -0,0 +1,25 @@
+﻿using LastBell.ViewModels;
+using LastBell.ViewModels.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvvmNavigationLib.Services.ServiceCollectionExtensions;
+using MvvmNavigationLib.Stores;
+
+namespace LastBell.HostBuilders
+{
+    public static class BuildMainNavigationExtension
+    {
+        public static IHostBuilder BuildMainNavigation(this IHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<NavigationStore>();
+                services.AddUtilityNavigationServices<NavigationStore>();
+                services.AddNavigationService<MainPageViewModel, NavigationStore>();
+
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ LastBell/ViewModels/Pages/MainPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LastBell.ViewModels.Pages
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+    }
+}
